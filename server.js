c396df47d2f181aed6035f66f6c439e6a24d09ab
mjurek1 <jurekmichael0@gmail.com>
2024-08-20 20:43:50 -0500

    fixing file structure

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const api_routes = require('./Develop/routes/api-routes');
-const html_routes = require('./Develop/routes/html-routes');
+// const api_routes = require('./Develop/routes/api-routes');
+// const html_routes = require('./Develop/routes/html-routes');
+// const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-app.use(api_routes);
-app.use(html_routes);
+// app.use(api_routes);
+// app.use(html_routes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.use(express.static('public'));
+
+app.listen(PORT, () =>
+    console.log(`Server is running on http://localhost:${PORT}`)
+);
